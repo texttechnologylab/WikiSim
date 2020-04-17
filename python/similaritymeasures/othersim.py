2a59b138985c2fc9ed870259e0b8047a082744aa
Pascal Welke <welke@uni-bonn.de>
2020-04-17 17:21:02 +0200

    there are nan values in the intersection_rw_kernel results.
    likely this is a result of an unhandled situation with an empty intersection vertex set.

--- python/similaritymeasures/othersim.py
+++ python/similaritymeasures/othersim.py
@@ -63,11 +63,62 @@ def intersection_rw_kernel(g1: igraph.Graph, g2: igraph.Graph, lamda=0.001, dire
     :return:
     """
     g = intersection_graph(g1, g2, directed=directed)
-    A = np.array(g.get_adjacency().data)
-    n = g.vcount()
-    S = np.linalg.inv(np.eye(n) - lamda * A)
-    # this computes the kernel for normalized uniform weight vertex weight vectors
-    return S.sum() / (n * n)
+    if g.vcount() > 0:
+        A = np.array(g.get_adjacency().data)
+        n = g.vcount()
+        S = np.linalg.inv(np.eye(n) - lamda * A)
+        # this computes the kernel for normalized uniform weight vertex weight vectors
+        return S.sum() / (n * n)
+    else:
+        return 0.0
+
+
+def intersection_rw_kernel_unnormalized(g1: igraph.Graph, g2: igraph.Graph, lamda=0.001, directed=True):
+    """
+    We compute the similarity of two wikipedias as the number of simultaneous random walks on them,
+    which is the number of random walks on the intersection graph, as both graphs are node aligned
+    :param g1: first graph
+    :param g2: second graph
+    :param lamda: convergence parameter of the random walk kernel. Must be set small enough. See
+    Gärtner, Wrobel, Flach: On graph kernels: Hardness results and efficient alternatives
+    or
+    S. V. N. Vishwanathan and Nicol N. Schraudolph and Risi Kondor and Karsten M. Borgwardt: Graph Kernels
+    :return:
+    """
+    g = intersection_graph(g1, g2, directed=directed)
+    if g.vcount() > 0:
+        A = np.array(g.get_adjacency().data)
+        n = g.vcount()
+        S = np.linalg.inv(np.eye(n) - lamda * A)
+        # this gives an unnormalized version. large intersections with many random walks win.
+        return S.sum()
+    else:
+        return 0.0
+
+
+def intersection_rw_kernel_kiter(g1: igraph.Graph, g2: igraph.Graph, k=10, directed=True):
+    """
+    We compute the similarity of two wikipedias as the number of simultaneous random walks of length k them,
+    which is the number of random walks on the intersection graph, as both graphs are node aligned
+    :param g1: first graph
+    :param g2: second graph
+    :param k: length of the random walk.
+
+    See
+    Gärtner, Wrobel, Flach: On graph kernels: Hardness results and efficient alternatives
+    or
+    S. V. N. Vishwanathan and Nicol N. Schraudolph and Risi Kondor and Karsten M. Borgwardt: Graph Kernels
+    :return:
+    """
+    g = intersection_graph(g1, g2, directed=directed)
+    if g.vcount() > 0:
+        A = np.array(g.get_adjacency().data)
+        n = g.vcount()
+        S = np.power(A, k)
+        # this computes the kernel for normalized uniform weight vertex weight vectors
+        return S.sum() / (n * n)
+    else:
+        return 0.0
 
 
 def computeGED(G1: igraph.Graph, G2: igraph.Graph):
