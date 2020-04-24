520e0d549a212663a12816d8821c553b2da9fa77
Pascal Welke <welke@uni-bonn.de>
2020-04-24 15:37:41 +0200

    Unification of the experiments for the random comparison

--- python/experiments_compare_to_random/all_experiments.sh
+++ python/experiments_compare_to_random/all_experiments.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-nohup python3 -u ../experiments/compare_to_random.py ../../graphs/ ../../outputComplete/random/ &
+nohup python3 -u ../experiments/compare_to_random_ER.py ../../graphs/ ../../outputComplete/random/ > IN.out &
 sleep 1
-nohup python3 -u ../experiments/compare_to_random.py ../../graphs/oecd/ ../../outputComplete/oecd/random/ &
+nohup python3 -u ../experiments/compare_to_random_ER.py ../../graphs/oecd/ ../../outputComplete/oecd/random/ > oecd.out &
 sleep 1
-nohup python3 -u ../experiments/compare_to_random.py ../../graphs/oecdTopics/ ../../outputComplete/oecdTopics/random/ &
+nohup python3 -u ../experiments/compare_to_random_ER.py ../../graphs/oecdTopics/ ../../outputComplete/oecdTopics/random/ > oecdTopics.out &
 sleep 1
