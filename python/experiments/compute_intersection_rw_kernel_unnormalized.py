ab2c212e2cd52fda755e56b7e49df7720f308e80
Pascal Welke <welke@uni-bonn.de>
2020-05-07 09:19:48 +0200

    allow to give type parameter via command line

--- python/experiments/compute_intersection_rw_kernel_unnormalized.py
+++ python/experiments/compute_intersection_rw_kernel_unnormalized.py
@@ -33,14 +33,23 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         dataset_root = os.path.join('..', '..', 'graphs')
         dataset_output = os.path.join('..', '..', 'output')
-    elif len(sys.argv) == 3:
+        gml_switch = 'all'
+    elif len(sys.argv) == 4:
         dataset_root = sys.argv[1]
         dataset_output = sys.argv[2]
+        gml_switch = sys.argv[3]
     else:
-        sys.stderr.write('either no or two args are required.\n '
+        sys.stderr.write('either no or three args are required.\n '
                          'usage: PROG dataset_root output_root\n')
         sys.exit(1)
 
-    gml_types = ['gml', 'fullgml']
+    if gml_switch == 'gml':
+        gml_types = ['gml']
+    if gml_switch == 'fullgml':
+        gml_types = ['fullgml']
+    if gml_switch == 'all':
+        gml_types = ['gml', 'fullgml']
+    if gml_switch == 'none':
+        gml_types = ['']
 
     run_experiment(dataset_root=dataset_root, dataset_output=dataset_output, gml_types=gml_types)
